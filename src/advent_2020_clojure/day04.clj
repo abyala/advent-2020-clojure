351942466fd1694c66e8bde533d54cad53c048fa
Andrew Byala <abyala@yahoo.com>
2020-12-04 10:17:30 -0600

    Day 4, part 2, pre-cleanup

--- src/advent_2020_clojure/day04.clj
+++ src/advent_2020_clojure/day04.clj
@@ -24,7 +24,47 @@
        (set/difference required-fields)
        empty?))
 
+(defn maybe-parse-int [s]
+  (try (Integer/parseInt s)
+       (catch NumberFormatException _ nil)))
+
+(defn in-range? [min max s]
+  (when-let [i (maybe-parse-int s)]
+    (<= min i max)))
+
+(defn birth-year? [s] (in-range? 1920 2002 s))
+(defn issue-year? [s] (in-range? 2010 2020 s))
+(defn expiration-year? [s] (in-range? 2020 2030 s))
+(defn height? [s] (when-let [[_ amt unit] (re-matches #"(\d+)(in|cm)" s)]
+                    (case unit
+                      "cm" (<= 150 (Integer/parseInt amt) 193)
+                      "in" (<= 59 (Integer/parseInt amt) 76))))
+(defn hair-color? [s] (some? (re-matches #"\#[0-9a-f]{6}" s)))
+(defn eye-color? [s] (#{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} s))
+(defn passport-id? [s] (re-matches #"[\d]{9}" s))
+
+(def validation-checks {"byr" birth-year?
+                        "iyr" issue-year?
+                        "eyr" expiration-year?
+                        "hgt" height?
+                        "hcl" hair-color?
+                        "ecl" eye-color?
+                        "pid" passport-id?})
+
+(defn passes-check? [[k v]]
+  (if-let [f (validation-checks k)]
+    (f v)
+    true))
+
+(defn valid-passport2 [passport]
+  (every? passes-check? passport))
+
 (defn part1 [input]
   (->> (parse-input input)
        (filter valid-passport?)
+       count))
+
+(defn part2 [input]
+  (->> (parse-input input)
+       (filter #(and (valid-passport? %) (valid-passport2 %)))
        count))
--- test/advent_2020_clojure/day04_test.clj
+++ test/advent_2020_clojure/day04_test.clj
@@ -9,3 +9,16 @@
 (deftest part1-test
   (is (= 2 (part1 TEST_DATA)))
   (is (= 170 (part1 PUZZLE_DATA))))
+
+(deftest valid-passport2-test
+  (is (false? (valid-passport2 (parse-passport "eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926"))))
+  (is (false? (valid-passport2 (parse-passport "iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946"))))
+  (is (false? (valid-passport2 (parse-passport "hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277"))))
+  (is (false? (valid-passport2 (parse-passport "hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007"))))
+  (is (valid-passport2 (parse-passport "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f")))
+  (is (valid-passport2 (parse-passport "eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm")))
+  (is (valid-passport2 (parse-passport "hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022")))
+  (is (valid-passport2 (parse-passport "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"))))
+
+(deftest part2-test
+  (is (= 103 (part2 PUZZLE_DATA))))
