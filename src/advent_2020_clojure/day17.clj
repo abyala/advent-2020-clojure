916e38ec7f42c07d053e01dabd5a0504b1abcc2d
Andrew Byala <abyala@yahoo.com>
2020-12-17 17:21:22 -0600

    Day 17 part 2

--- src/advent_2020_clojure/day17.clj
+++ src/advent_2020_clojure/day17.clj
@@ -1,16 +1,17 @@
 (ns advent-2020-clojure.day17
   (:require [clojure.string :as str]))
 
-(defn neighbors-of [[x y z]]
-  (for [new-x [-1 0 1]
+(defn neighbors-of [[x y z w] four-d?]
+  (for [new-w (if four-d? [-1 0 1] [0])
+        new-x [-1 0 1]
         new-y [-1 0 1]
         new-z [-1 0 1]
-        :when (some #(not= 0 %) [new-x new-y new-z])]
-    [(+ x new-x) (+ y new-y) (+ z new-z)]))
+        :when (some #(not= 0 %) [new-x new-y new-z new-w])]
+    [(+ x new-x) (+ y new-y) (+ z new-z) (+ w new-w)]))
 
 (defn parse-input [input]
   (->> (str/split-lines input)
-       (map-indexed (fn [y row] (map-indexed (fn [x c] [[x y 0] c])
+       (map-indexed (fn [y row] (map-indexed (fn [x c] [[x y 0 0] c])
                                              row)))
        (apply concat)
        (keep (fn [[coords v]] (when (= v \#) coords)))
@@ -20,33 +21,38 @@
   (mapv (fn [offset]
           (let [values (map #(get % offset) active-points)]
             [(dec (apply min values)) (inc (apply max values))]))
-        [0 1 2]))
+        [0 1 2 3]))
 
 (defn inclusive-range [[low high]]
   (range low (inc high)))
 
-(defn points-in-cube [[cube-x cube-y cube-z]]
+(defn points-in-cube [[cube-x cube-y cube-z cube-w]]
   (for [x (inclusive-range cube-x)
         y (inclusive-range cube-y)
-        z (inclusive-range cube-z)]
-    [x y z]))
+        z (inclusive-range cube-z)
+        w (inclusive-range cube-w)]
+    [x y z w]))
 
-(defn num-active-neighbors [active-points point]
-  (->> point neighbors-of (filter (partial active-points)) count))
+(defn num-active-neighbors [active-points four-d? point]
+  (->> (neighbors-of point four-d?)
+       (filter #(active-points %))
+       count))
 
-(defn next-point [active-points point]
-  (let [actives (num-active-neighbors active-points point)]
+(defn next-point [active-points four-d? point]
+  (let [actives (num-active-neighbors active-points four-d? point)]
     (if (active-points point)
       (contains? #{2 3} actives)
       (= 3 actives))))
 
-(defn next-board [active-points]
+(defn next-board [active-points four-d?]
   (->> active-points
        bounding-cube
        points-in-cube
-       (keep #(when (next-point active-points %) %))
+       (keep #(when (next-point active-points four-d? %) %))
        set))
 
 (defn part1 [input]
-  (count (nth (iterate next-board (parse-input input)) 6)))
+  (count (nth (iterate #(next-board % false) (parse-input input)) 6)))
 
+(defn part2 [input]
+  (count (nth (iterate #(next-board % true) (parse-input input)) 6)))
--- test/advent_2020_clojure/day17_test.clj
+++ test/advent_2020_clojure/day17_test.clj
@@ -5,22 +5,10 @@
 (def TEST_DATA ".#.\n..#\n###")
 (def PUZZLE_DATA (slurp "resources/day17_data.txt"))
 
-(deftest num-active-neighbors-test
-  (let [actives #{[1 0 0] [2 2 0] [0 2 0] [1 2 0] [2 1 0]}]
-    (is (= 1 (num-active-neighbors actives [0 0 0])))
-    (is (= 1 (num-active-neighbors actives [1 0 0])))
-    (is (= 2 (num-active-neighbors actives [2 0 0])))
-    (is (= 3 (num-active-neighbors actives [0 1 0])))
-    (is (= 5 (num-active-neighbors actives [1 1 0])))
-    (is (= 3 (num-active-neighbors actives [2 1 0])))
-    (is (= 1 (num-active-neighbors actives [0 0 1])))
-    (is (= 5 (num-active-neighbors actives [1 1 1])))
-    (is (= 2 (num-active-neighbors actives [1 0 1])))))
-
 (deftest part1-test
   (is (= 112 (part1 TEST_DATA)))
   (is (= 375 (part1 PUZZLE_DATA))))
 
-#_(deftest part2-test
+(deftest part2-test
   (is (= 848 (part2 TEST_DATA)))
-  )
+  (is (= -1 (part2 PUZZLE_DATA))))
